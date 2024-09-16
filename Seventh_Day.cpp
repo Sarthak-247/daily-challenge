f4c353213873a5d2e549ddf6c83b7e4f076e9357
Sarthak-247 <deochakesarthak50@gmail.com>
2024-09-16 12:16:22 +0530

    Seventh_Day.cpp

--- Seventh_Day.cpp
+++ Seventh_Day.cpp
@@ -1,21 +1,32 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-int bucket(int a[],int n)
+int trap_water(vector<int>& height)
 {
-    int count=0;
+    int n=height.size();
+    if(n==0)    return 0;
+    vector<int> leftmax(n),rightmax(n);
+    leftmax[0]=height[0];
     for(int i=0;i<n;i++)
     {
-        if(a[i]!=0 && a[i+1]!=0)
-        {
-            count+=1;
-        }
+        leftmax[i]=max(leftmax[i-1],height[i]);
     }
-    return count;
+    rightmax[n-1]=height[n-1];
+    for(int i=n-2;i>=0;i--)
+    {
+        rightmax[i]=max(rightmax[i+1],height[i]);
+    }
+    int trap_water_unit=0;
+    for(int j=0;j<n;j++)
+    {
+        trap_water_unit+= min(leftmax[j], rightmax[j])- height[j];
+    }
+    return trap_water_unit;
 }
 int main()
 {
-    int a[]={0,1,0,2,1,0,1,3,2,1,2,1};
-    int n=sizeof(a)/sizeof(a[0]);
-    int result=bucket(a,n);
-    cout<<result;
+    vector<int> height={0,1,0,2,1,0,1,3,2,1,2,1};
+    cout<<trap_water(height);
+    return 0;
 }
